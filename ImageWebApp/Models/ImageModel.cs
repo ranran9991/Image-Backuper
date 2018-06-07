d9fe5ebc1b446f3bacce0aac73e63d3cc4cfa8f2
rongever <33871627+rongever@users.noreply.github.com>
2018-06-07 15:39:56 +0300

    image model interface implemented

--- ImageWebApp/Models/ImageModel.cs
+++ ImageWebApp/Models/ImageModel.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -12,14 +14,17 @@ namespace ImageWebApp.Models
 
         public ImageModel()
         {
-            thumbnails = new List<Thumbnail>
+            /*thumbnails = new List<Thumbnail>
             {
                 new Thumbnail("img0", "\\images\\image.jpg",2, 2012),
                 new Thumbnail("img1" ,"\\images\\image1.jpg", 3, 1996),
                 new Thumbnail("img2", "\\images\\image2.jpeg", 5, 2001),
                 new Thumbnail("img3", "\\images\\image3.jpg", 12, 1832),
                 new Thumbnail("img3", "\\images\\image4.jpg", 9, 1771)
-            };
+            }; */
+            // i left it here for your use
+
+            thumbnails = GetThumbnails();
         }
         public string ThumbnailsToJSON()
         {
@@ -34,26 +39,70 @@ namespace ImageWebApp.Models
             thumbList["Thumbnails"] = arr;
             return thumbList.ToString();
         }
-        /// <summary>
-        /// given the thumbnail path returns the real image relative path
-        /// </summary>
-        /// <param name="thumbPath"></param>
-        /// <returns></returns>
-        public string GetRealImagePath(string thumbPath)
+
+        public List<Thumbnail> GetThumbnails()
         {
-            return "\\images\\image2.jpeg";
-        }
-        /// <summary>
-        /// Removes the picture and the thumbnail given the thumbnail path
-        /// </summary>
-        /// <param name="path"></param>
-        public void RemovePicture(string path)
-        {   
-            foreach(Thumbnail thumb in thumbnails)
+            List<Thumbnail> thumbs = new List<Thumbnail>();
+            string outputDir = ConfigurationManager.AppSettings["OutputDir"];
+            string thumbDirPath = outputDir + "\\Thumbnails";
+            Console.WriteLine(thumbDirPath);
+
+            string[] years = Directory.GetDirectories(outputDir);
+
+            foreach (string year in years)
             {
-                thumbnails.Clear();
+                if (year != thumbDirPath)
+                {
+                    string[] months = Directory.GetDirectories(year);
+
+                    foreach (string month in months)
+                    {
+                        string[] images = Directory.GetFiles(month);
+
+                        foreach (string image in images)
+                        {
+                            string name = image.Substring(month.Length + 1);
+                            string path = thumbDirPath + "\\" + name;
+                            int _month = int.Parse(month.Substring(year.Length + 1));
+                            int _year = int.Parse(year.Substring(outputDir.Length + 1));
+
+                            Thumbnail thumbnail = new Thumbnail(name, path, _month, _year);
+                            thumbs.Add(thumbnail);
+                        }
+                    }
+                }
             }
+            return thumbs;
         }
 
+        public string GetRealImagePath(string thumbPath)
+        {
+            foreach (Thumbnail thumb in thumbnails)
+            {
+                if (thumb.Path == thumbPath)
+                {
+                    return ConfigurationManager.AppSettings["OutputDir"] + "\\" + thumb.Year +
+                        "\\" + thumb.Month + "\\" + thumb.Name;
+                }
+            }
+            return "";
+        }
+
+        public void RemovePicture(string path)
+        {
+            string realImage = GetRealImagePath(path);
+            if (realImage == "") return;
+            File.Delete(path);
+            File.Delete(realImage);
+
+            foreach (Thumbnail thumb in thumbnails)
+            {
+                if (thumb.Path == path)
+                {
+                    thumbnails.Remove(thumb);
+                    break;
+                }
+            }
+        }
     }
 }
